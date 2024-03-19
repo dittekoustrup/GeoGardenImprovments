9a7ee344e84e1621704110f4db686ffb7f00098b
KasperPtak <kasperptak2@gmail.com>
2024-03-19 14:42:24 +0100

    Homepage hero and intro component created

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { useRoute, useRouter } from "vue-router";
+import IntroSubscription from '@/components/IntroSubscription.vue'
+
 const router = useRouter();
 const routes = router.getRoutes();
 </script>
@@ -12,5 +14,39 @@ const routes = router.getRoutes();
         <RouterLink :to="r.path">{{ r.meta.title }}</RouterLink>
       </li>
     </menu>
+
+    <section>
+      <div class="hero">
+        <img class="hero__image" src="https://placehold.co/600x600" alt="#">
+      </div>
+    </section>
+
+    <IntroSubscription 
+      title="Velkommen til haven"
+      body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem exercitationem quaerat odio voluptate mollitia totam. Accusantium explicabo autem illo est dolor maxime culpa velit, temporibus cum id quam fugitomnis placeat! Numquam assumenda repudiandae distinctio sint nostrum, cum dicta! Eius quae architecto corporis numquam sed dolor voluptates velit doloribus cum harum voluptate doloremque dignissimos deserunt molestiae repellat, perferendis magni, veritatis quia officiis eligendi. Fuga harum esse numquam iste eaque id possimus, iure perspiciatis animi. Sunt iure nesciunt perferendis sequi."
+    />
+
   </main>
 </template>
+
+<style lang="scss" scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.hero {
+  width: 1517px;
+  height: 524px;
+
+  &__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+</style>
+
+<!-- TODO: Make/move introsection into own component -->
