80f1297c0f48812224a2ecc228ec5c865b59edc1
soklope <skp2104@hotmail.com>
2024-03-21 13:35:18 +0100

    Navbar setup - Needs dropdown, and SCSS variables

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { RouterView } from "vue-router";
+import { RouterLink, RouterView } from "vue-router";
+import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
+  <Navigation />
   <RouterView />
 </template>
 
