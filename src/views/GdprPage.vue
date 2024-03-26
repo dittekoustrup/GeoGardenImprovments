7d6db4ad421f2aa96a4e2a0ce3c0dae89deb2856
dittekoustrup <dittekoustrup@outlook.com>
2024-03-26 13:01:39 +0100

    Add GDPR page and global styling

---
+++ src/views/GdprPage.vue
@@ -0,0 +1,186 @@
+<template>
+    <div>
+        <div class="hero-section">
+            <img src="@/assets/images/gdpr_top_image.webp" alt="Child playing in the meadow with soap bubbles">
+        </div>
+        <div class="content-section">
+            <h2>Behandling af personoplysninger</h2>
+            <p>Som et led i Geografisk Haves aktiviteter og markedsføring, indsamles og behandles visse oplysninger om
+                vores
+                gæster i haven og besøgende på vores hjemmeside. Denne politik beskriver, hvordan Geografisk Have
+                behandler
+                disse oplysninger.
+            </p>
+            <p>
+                <strong>Dataansvarlig</strong><br>
+                Den selvejende institution Geografisk Have CVR. 36675802, Chr. 4. vej 23, 6000 Kolding. Tlf. 79797000.
+            </p>
+            <p>
+                <strong>Indsamling af data</strong><br>
+                Indsamling og lagring af data sker blandt andet, når du besøger vores hjemmeside, www.geografiskhave.dk
+                eller når du tilmelder dig vores nyhedsbrev. De oplysninger, som vi gemmer, kan være navn, e-mailadresse
+                og
+                telefonnummer, samt hvordan du navigerer rundt på vores hjemmeside.
+                <br><br>
+                Når du køber produkter relateret til Geografisk Have på Kolding Kommunes webshop, for eksempel årskort,
+                opbevarer vi også nogle af dine data i en periode. Disse bliver dog slettet, når de ikke bruges længere.
+                <br><br>
+                Som fysisk gæst i Geografisk Have indsamler vi udelukkende anonymiserede data så som postnummer eller
+                landekoder om, hvor du kommer fra.
+            </p>
+            <p>
+                <strong>Formål og brug</strong><br>
+                Vi indsamler data for at kunne optimere gæsternes oplevelse i haven og yde en god service. Geografisk
+                Have
+                behandler dine persondata til følgende formål:
+                <br><br>
+                <em>Gennemførsel af ordre i webshop</em><br>
+                Når du køber et produkt relateret til Geografisk Have i Kolding Kommuneswebshop, beder vi dig om navn,
+                adresse, fødselsdato og e-mail adresse. Disse oplysninger bruges udelukkendetil at gennemføre din ordre
+                og
+                til at sende dig en kvittering. Oplysningerne bruges ikke til andre formål end dette.
+                <br><br>
+                <em>Køb af Årskort</em><br>
+                Når du køber et årskort i vores reception, skal du udfylde en blanket hvorfra anonymiserede
+                data gemmes, såsom køn, alder og postnummer. Disse oplysninger bruges til markedsføring og tage hensyn
+                til
+                gæsternes præferencer. Vi gemmer ikke dine data længere, end vi har brug for dem.
+                <br><br>
+                <em>Køb eller bestilling af oplevelser, rundvisninger eller leje af faciliteter</em><br>
+                Når du bestiller et produkt, en oplevelse eller lejer faciliteter i haven, afgiver du forskellige
+                informationer for, at vi kan yde den ønskede service. Det kan være navn, telefonnummer og e-mailadresse.
+                Disse oplysninger bruges til at udføreden nødvendige service du har bedt om. Vi gemmer ikke dine data
+                længere, end vi har brug for dem. I vissetilfælde anonymiseres visse data til brug i statistik, så vi
+                kan se
+                hvilke arrangementer og events vi harlavet og på hvilke tidspunkter. Disse oplysninger vil dog aldrig
+                kunne
+                føres tilbage til enkeltpersoner.
+                <br><br>
+                <em>Nyhedsbreve</em><br>
+                Nyhedsbreve sendes kun ud til personer, der har givet deres samtykke hertil. For eksempel gennem
+                tilmelding via Geografisk Haves hjemmeside. De oplysninger der hentes er navn og e-mailadresse. Du kan
+                til
+                enhver tid afmelde nyhedsbrevet ved at skrive til os på info@geografiskhave.dk eller via linket i bunden
+                af
+                nyhedsbrevet.
+                <br><br>
+                <em>Hjemmesiden</em><br>
+                Geografisk Have har adgang til visse oplysninger om besøgendes brug af hjemmesiden og dennes
+                funktioner.Ved besøg på Geografisk Haves hjemmeside indsamler vi følgende oplysninger om dig:
+                <br>
+            <ul>
+                <li>Hvilke sider du besøger på geografiskhave.dk samt hvornår og hvor længe</li>
+                <li>Browsertype</li>
+                <li>IP - adresse.</li>
+            </ul>
+            </p>
+            <p>
+                De indsamlede data behandles på grundlag af Geografisk Haves interesse i at få kendskab til brugernes
+                anvendelse af hjemmesiden. Informationen bruges til at forbedre hjemmesiden. Vi bruger derudover data om
+                vores bruger til at forstå, hvordan vores brugere generelt anvender hjemmesiden og prøver på baggrund af
+                disse informationer at gøre hjemmesiden bedre.Vi kan ikke se, hvilke sider du kommer fra - medmindre du
+                kommer fra en Google søgning eller via Facebook link - eller hvor du surfer hen efter besøget på vores
+                hjemmeside. Desuden indsamler vi informationer om, hvilke devices vores brugere generelt foretrækker.
+                Denne
+                information bruger vi også til at forbedre vores hjemmeside.
+                <br><br>
+                <em>Facebook Pixel tracking</em><br>
+                Geografisk Have bruger Facebook Pixel tracking, som indsamler anonyme oplysninger om
+                besøg til og fra vores hjemmeside via Facebook.
+            </p>
+            <p>
+                <strong>Ansatte, bestyrelsesmedlemmer og samarbejdspartnere</strong><br>
+                Geografisk Have gemmer oplysninger på ansatte og samarbejdspartnere så længe at det er relevant eller
+                indtil
+                samarbejder og ansættelsesforhold ophører.
+            </p>
+            <p>
+                <strong>Ansatte, bestyrelsesmedlemmer og samarbejdspartnere</strong><br>
+                Geografisk Have gemmer oplysninger på ansatte og samarbejdspartnere så længe at det er relevant eller
+                indtil
+                samarbejder og ansættelsesforhold ophører.
+            </p>
+            <p>
+                <strong>Evt. videregivelse af oplysninger til 3.part</strong><br>
+                Geografisk Have videregiver ikke personoplysninger til tredjepart.
+            </p>
+            <p>
+                <strong>Sikkerhed</strong><br>
+                Geografisk Have behandler alle personoplysninger fortroligt. Vi opbevarer ikke dine oplysninger længere
+                end
+                højst nødvendigt. Vi sletter løbende de oplysninger, som vi ikke længere bruger. Oplysningerne behandles
+                kun
+                af medarbejdere, der er instrueret i hvordan personoplysninger behandles.
+            </p>
+            <p>
+                <strong>Tilbagekaldelse af samtykke</strong><br>
+                Du kan til enhver tid tilbagekalde dit samtykke til brug af dine personoplysninger, som du har afgivet
+                Geografisk Have. Tilbagekaldelse af dine oplysninger kan dog betyde, at vi ikke længere kan tilbyde dig
+                de
+                samme ydelser og services. Tilbagekaldelse af samtykke skal ske skriftligt til:
+                <br>
+            <ul>
+                <li>E-mail til: info@geografiskhave.dk</li>
+                <li>Brev til: Geografisk Have, Christian 4 Vej 23, 6000 Kolding</li>
+            </ul>
+            </p>
+            <p>
+                <strong>Dine rettigheder</strong><br>
+                Du har ret til at få indsigt i, hvilke oplysninger Geografisk Have behandler om dig og til hvilket
+                formål
+                oplysningerne behandles. Du har desuden ret til at gøre indsigelse mod, at dine personoplysninger
+                behandles
+                eller kræve berigtigelse af oplysninger, der er ukorrekte, vildledende eller på anden måde behandles i
+                strid
+                med lovgivningen.
+            </p>
+            <p>
+                <strong>Ændringer i persondatapolitikken</strong><br>
+                Geografisk Have er en virksomhed i udvikling og vores persondatapolitik opdateres løbende i takt med de
+                nye
+                tiltag, der eventuelt måtte komme i fremtiden.
+                <br><br>
+                Hvis du har spørgsmål eller ønsker at klage over behandlingen af personoplysninger, kan du kontakte
+                Geografisk Have via mail eller telefon.
+            <ul>
+                <li>Telefon: 79 79 70 00</li>
+                <li>E-mail: info@geografiskhave.dk</li>
+                <li>Brev: Geografisk Have, Christian 4 Vej 23, 6000 Kolding</li>
+            </ul>
+            </p>
+            <p>
+                Udarbejdet af:<br>
+                <strong>Geografisk Have</strong><br>
+                <strong>Christian 4 Vej 23</strong><br>
+                <strong>6000 Kolding</strong><br>
+                <strong>CVR: 36675802</strong><br>
+            </p>
+        </div>
+        <div class="">
+            <img src="@/assets/images/gdpr_bottom_image.webp" alt="Kindergarten enjoying the garden">
+        </div>
+    </div>
+</template>
+
+<style>
+.content-section {
+    padding: 0px, 45px;
+    gap: 20px;
+    background-color: hotpink;
+}
+
+h2 {
+    font-size: 30px;
+    line-height: 70px;
+}
+
+p {
+    font-size: 14px;
+    line-height: 28px;
+}
+
+li {
+    font-size: 14px;
+    line-height: 28px;
+}
+</style>
