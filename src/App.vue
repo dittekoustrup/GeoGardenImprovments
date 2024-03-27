24a28ce423cfd32b9d7603a34092d46d7f9ed6e6
soklope <skp2104@hotmail.com>
2024-03-27 10:59:21 +0100

    Add footer componenet - still needs global styling from Ditte - Wrong branch, i know, forgot to change :))))))

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navigation from './components/Navigation.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <Navigation />
   <RouterView />
+  <Footer />
 </template>
