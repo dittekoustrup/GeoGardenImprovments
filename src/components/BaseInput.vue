e715968c83dcaa0c8cf5ed0a367c3d01c8736d6d
Annica <annif011098@gmail.com>
2024-04-05 17:01:54 +0200

    remove placeholder when focus

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="input-wrap">
-    <label v-if="label"> {{ label }}</label>
+  <div class="input-wrap" :class="{ active: modelValue !== '' }">
+    <label>{{ label }}</label>
     <input
       type="text"
       :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
+      @input="updateModelValue"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+      :placeholder="isFocused ? '' : label"
       v-bind="$attrs"
+      class="max-width-input my-input"
     />
   </div>
 </template>
+
 <script setup>
+import { ref } from "vue";
+
+const isFocused = ref(false);
+
 defineProps({
   label: {
     type: [String, Boolean],
@@ -20,16 +29,46 @@ defineProps({
     default: "",
   },
 });
+
+function updateModelValue(event) {
+  $emit("update:modelValue", event.target.value);
+}
 </script>
 
 <style lang="scss">
 .input-wrap {
   display: flex;
   flex-direction: column;
+  border-bottom: 1px solid black;
+  position: relative;
+
+  label {
+    font-size: 16px;
+    color: black;
+    padding-bottom: 2px;
+  }
 
   input {
-    padding: 8px 12px;
+    padding: 8px 0px;
     font-size: 16px;
+    border: none;
+    outline: none;
+  }
+
+  &.active label,
+  .my-input:focus::placeholder {
+    display: none;
+  }
+
+  .max-width-input {
+    max-width: 558px;
+  }
+
+  @media (min-width: 768px) {
+    .max-width-input {
+      min-width: 360px;
+      padding-bottom: 1px;
+    }
   }
 }
 </style>
