63ed2507ff70ea6dacc75f96c35d36e0556aecf4
Annica <annif011098@gmail.com>
2024-04-12 14:55:12 +0200

    signupform - add style

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,3 +1,44 @@
+<script setup>
+import { reactive, ref } from "vue";
+import { required, helpers } from "@vuelidate/validators";
+import BaseInput from "./BaseInput.vue";
+import useVuelidate from "@vuelidate/core";
+import SignUpModal from "./SignUpModal.vue";
+
+const formData = reactive({
+  firstName: "",
+  lastName: "",
+  email: "",
+  reEmail: "",
+  zipCode: "",
+  adult: "",
+  children: "",
+  agree: false,
+});
+
+const errorMessage = "Forkert indtastning";
+
+const rules = {
+  firstName: { required: helpers.withMessage(errorMessage, required) },
+  lastName: { required: helpers.withMessage(errorMessage, required) },
+  email: { required: helpers.withMessage(errorMessage, required) },
+  reEmail: { required: helpers.withMessage(errorMessage, required) },
+  agree: { required },
+};
+
+const v$ = useVuelidate(rules, formData);
+
+const modalOpen = ref(false);
+
+const submitForm = async () => {
+  const result = await v$.value.$validate();
+  if (result && formData.agree) {
+    modalOpen.value = true;
+  } else {
+    v$.agree.$touch();
+  }
+};
+</script>
 <template>
   <form @submit.prevent="submitForm" class="form form--main">
     <div class="input-wrapper input-wrapper--main">
@@ -8,11 +49,7 @@
           :key="error.$uid"
           class="error-message error-message--main"
         >
-          {{
-            error.$message === "This field is required"
-              ? "Fornavn skal udfyldes"
-              : error.$message
-          }}
+          {{ error.$message }}
         </span>
       </div>
       <div class="input-column input-column--main">
@@ -98,160 +135,23 @@
     <div class="checkbox-wrapper">
       <input type="checkbox" id="agreeCheckbox" v-model="formData.agree" />
       <label for="agreeCheckbox"
-        >Jeg accepterer betingelserne og vilkårene for tilmelding.</label
-      >
+        >Er du indforstået med, at I indsamler og behandler mine data i
+        overensstemmelse med jeres privatlivspolitik. Læs mere og vores cockie-
+        og privatlivspolitik.
+      </label>
       <span
         class="error-message error-message--main"
         v-if="!formData.agree && v$.agree.$dirty"
         >Du skal acceptere betingelserne og vilkårene.</span
       >
     </div>
-
-    <button type="submit" class="button button--main">Tilmeld</button>
-    <Modal
-      :show="modalOpen"
-      :onClose="closeModal"
-      class="modal modal--main"
-    ></Modal>
-    <Modal
-      :show="modalOpen"
-      @close="modalOpen = false"
-      class="modal modal--main"
-    >
-      <h2 class="modal-title modal-title--main">Du er nu tilmeldt!</h2>
-      <p class="modal-message modal-message--main">Tak for din tilmelding.</p>
-    </Modal>
+    <div class="button-container">
+      <button type="submit" class="button button--main">Tilmeld</button>
+    </div>
   </form>
+  <SignUpModal :show="modalOpen" />
 </template>
 
-<script setup>
-import { reactive } from "vue";
-import { ref } from "vue";
-import BaseInput from "../components/BaseInput.vue";
-import useVuelidate from "@vuelidate/core";
-import { required, helpers } from "@vuelidate/validators";
-import Modal from "./SignUpModal.vue";
-
-const formData = reactive({
-  firstName: "",
-  lastName: "",
-  email: "",
-  reEmail: "",
-  zipCode: "",
-  adult: "",
-  children: "",
-  agree: false,
-});
-
-const errorMessage = "Forkert indtastning";
-
-const rules = {
-  firstName: {
-    required: helpers.withMessage(errorMessage, required),
-  },
-  lastName: { required: helpers.withMessage(errorMessage, required) },
-  email: { required: helpers.withMessage(errorMessage, required) },
-  reEmail: { required: helpers.withMessage(errorMessage, required) },
-  agree: { required },
-};
-
-const v$ = useVuelidate(rules, formData);
-
-const modalOpen = ref(false);
-
-const submitForm = async () => {
-  const result = await v$.value.$validate();
-  if (result && formData.agree) {
-    modalOpen.value = true;
-  } else {
-    v$.agree.$touch();
-  }
-};
-</script>
 <style lang="scss" scoped>
-.form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.input-wrapper {
-  display: flex;
-  flex-direction: column;
-}
-
-.input-column {
-  margin: 0;
-}
-
-.input {
-  max-width: 100%;
-  padding-top: 50px;
-}
-
-.error-message {
-  color: red;
-}
-
-.button {
-  background-color: green;
-  color: white;
-  padding: 10px 20px;
-  width: 306px;
-  height: 66px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.modal {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: rgba(0, 0, 0, 0.5);
-  padding: 20px;
-  border-radius: 10px;
-  z-index: 1000;
-  width: 90%;
-}
-
-.modal-title {
-  color: white;
-  font-size: 24px;
-  margin-bottom: 10px;
-}
-
-.modal-message {
-  color: white;
-  font-size: 18px;
-}
-
-@media (min-width: 768px) {
-  .input-wrapper {
-    flex-direction: row;
-    gap: 59px;
-  }
-
-  .input-column {
-    flex: 1;
-  }
-}
-
-@media (min-width: 1024px) {
-  .form {
-    max-width: 800px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-
-  .button {
-    width: 280px;
-    height: 56px;
-  }
-
-  .modal {
-    width: 600px;
-  }
-}
+@import "../assets/style/SignUpForm.scss";
 </style>
