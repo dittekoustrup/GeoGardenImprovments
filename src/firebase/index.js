ebbd1adee07db2073da689f15f6a78d03b98e481
Annica <annif011098@gmail.com>
2024-04-15 13:38:49 +0200

    Firebase setup

---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZczsi1FcWr1W64qYxemQ9I4bAeu8D0AU",
+  authDomain: "geo-garden.firebaseapp.com",
+  projectId: "geo-garden",
+  storageBucket: "geo-garden.appspot.com",
+  messagingSenderId: "811667282253",
+  appId: "1:811667282253:web:7dfc1535b218460ad87f04",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
