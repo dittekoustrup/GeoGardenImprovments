60854583a53cd4b60eb3445c52773cb792fd2b23
Annica <annif011098@gmail.com>
2024-04-15 13:38:41 +0200

    Firestore import

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -4,6 +4,8 @@ import { required, helpers } from "@vuelidate/validators";
 import BaseInput from "./BaseInput.vue";
 import useVuelidate from "@vuelidate/core";
 import SignUpModal from "./SignUpModal.vue";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 const formData = reactive({
   firstName: "",
@@ -34,9 +36,29 @@ const updateShowModal = (value) => {
   modalOpen.value = value;
 };
 
+// Funktion til at sende formdata til Firestore
+const sendFormDataToFirestore = async (formData) => {
+  try {
+    // Tilføj en ny dokument til "userData" -samlingen
+    const docRef = await addDoc(collection(db, "userData"), {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      zipCode: formData.zipCode,
+      adult: formData.adult,
+      children: formData.children,
+      agree: formData.agree,
+    });
+    console.log("Dokument med ID:", docRef.id, "tilføjet til Firestore");
+  } catch (error) {
+    console.error("Fejl ved tilføjelse af dokument:", error);
+  }
+};
+
 const submitForm = async () => {
   const result = await v$.value.$validate();
   if (result && formData.agree) {
+    await sendFormDataToFirestore(formData);
     modalOpen.value = true;
   } else {
     v$.agree.$touch();
