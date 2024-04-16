2d7ad1fd9ca5ae4790ebd521464a6bbc97c75fea
KasperPtak <kasperptak2@gmail.com>
2024-04-16 13:25:14 +0200

    error message refactoring + styling

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -43,132 +43,65 @@ const submitForm = async () => {
   }
 };
 </script>
+
 <template>
   <form @submit.prevent="submitForm" class="form form--main">
     <div class="input-wrapper input-wrapper--main">
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.firstName"
-          type="text"
-          label="Fornavn"
-          placeholder="Fornavn"
-          class="input input--main"
-        />
-        <span
-          v-for="error in v$.firstName.$errors"
-          :key="error.$uid"
-          class="error-message error-message--main"
-        >
-          {{ error.$message }}
-        </span>
+        <BaseInput v-model="formData.firstName" type="text" label="Fornavn"
+          :placeholder="v$.firstName.$error ? errorMessage : 'Fornavn'"
+          :class="v$.firstName.$error ? 'error-message' : ''" class="input input--main" />
       </div>
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.lastName"
-          type="text"
-          label="Efternavn"
-          placeholder="Efternavn"
-          class="input input--main"
-        />
-        <span
-          v-for="error in v$.lastName.$errors"
-          :key="error.$uid"
-          class="error-message error-message--main"
-        >
-          {{ error.$message }}
-        </span>
+        <BaseInput v-model="formData.lastName" type="text" label="Efternavn" class="input input--main"
+          :placeholder="v$.firstName.$error ? errorMessage : 'Efternavn'"
+          :class="v$.lastName.$error ? 'error-message' : ''" />
+
       </div>
     </div>
     <div class="input-wrapper input-wrapper--main">
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.zipCode"
-          type="number"
-          label="Postnummer"
-          placeholder="Postnummer"
-          class="input input--main"
-        />
+        <BaseInput v-model="formData.zipCode" type="number" label="Postnummer" placeholder="Postnummer"
+          class="input input--main" />
       </div>
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.email"
-          type="email"
-          label="Mail"
-          placeholder="Email"
-          class="input input--main"
-        />
-        <span
-          v-for="error in v$.email.$errors"
-          :key="error.$uid"
-          class="error-message error-message--main"
-        >
-          {{ error.$message }}
-        </span>
+        <BaseInput v-model="formData.email" type="email" label="Mail" class="input input--main"
+          :placeholder="v$.firstName.$error ? errorMessage : 'Email'" :class="v$.email.$error ? 'error-message' : ''" />
       </div>
     </div>
     <div class="input-wrapper input-wrapper--main">
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.reEmail"
-          type="email"
-          label="Gentag email"
-          placeholder="Gentag email"
-          class="input input--main"
-        />
-        <span
-          v-for="error in v$.reEmail.$errors"
-          :key="error.$uid"
-          class="error-message error-message--main"
-        >
-          {{ error.$message }}
-        </span>
+        <BaseInput v-model="formData.reEmail" type="email" label="Gentag email" placeholder="Gentag email"
+          class="input input--main" :placeholder="v$.reEmail.$error ? errorMessage : 'Gentag email'"
+          :class="v$.email.$error ? 'error-message' : ''" />
       </div>
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.phone"
-          type="number"
-          label="Mobilnummer"
-          placeholder="Mobilnummer"
-          class="input input--main"
-        />
+        <BaseInput v-model="formData.phone" type="number" label="Mobilnummer" placeholder="Mobilnummer"
+          class="input input--main" />
       </div>
     </div>
     <div class="input-wrapper input-wrapper--main">
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.adult"
-          type="text"
-          label="Antal voksne i husstanden"
-          placeholder="Antal i husstanden"
-          class="input input--main"
-        />
+        <BaseInput v-model="formData.adult" type="text" label="Antal voksne i husstanden"
+          placeholder="Antal i husstanden" class="input input--main" />
       </div>
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.children"
-          type="text"
-          label="Antal børn i husstanden"
-          placeholder="Antal børn i husstanden"
-          class="input input--main"
-        />
+        <BaseInput v-model="formData.children" type="text" label="Antal børn i husstanden"
+          placeholder="Antal børn i husstanden" class="input input--main" />
       </div>
     </div>
 
     <div class="checkbox-wrapper">
       <input type="checkbox" id="agreeCheckbox" v-model="formData.agree" />
-      <label for="agreeCheckbox"
-        >Er du indforstået med, at I indsamler og behandler mine data i
+      <label for="agreeCheckbox">Er du indforstået med, at I indsamler og behandler mine data i
         overensstemmelse med jeres privatlivspolitik. Læs mere og vores cockie-
         og privatlivspolitik.
       </label>
-      <span
-        class="error-message error-message--main"
-        v-if="!formData.agree && v$.agree.$dirty"
-        >Du skal acceptere betingelserne og vilkårene.</span
-      >
     </div>
+    <span class="error-span" v-if="!formData.agree && v$.agree.$dirty"> <br> Du skal acceptere
+      betingelserne og vilkårene.</span>
     <div class="button-container">
-      <button type="submit" class="button button--main">Tilmeld</button>
+      <button type="submit" class="button button--main">Bliv medlem</button>
     </div>
   </form>
   <SignUpModal :show="modalOpen" @update:show="updateShowModal" />
