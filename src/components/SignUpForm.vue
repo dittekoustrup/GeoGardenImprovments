4812d5b15dd329e778a91710a74f8cac2390e1da
KasperPtak <kasperptak2@gmail.com>
2024-04-24 21:06:24 +0200

    add password reset page, update resetlogic in service, add heading to newsletter form

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -74,104 +74,72 @@ const submitForm = async () => {
 </script>
 
 <template>
+
+  <div class="form-heading">
+    <h2>
+      Bliv en del af klubben
+    </h2>
+    <p>
+      Velkommen til Geografisk Havens kundeklub! Som medlem af vores kundeklub får du adgang til en verden af eksklusive
+      fordele.
+    </p>
+  </div>
+
   <form @submit.prevent="submitForm" class="form form--main">
     <div class="input-wrapper input-wrapper--main">
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.firstName"
-          type="text"
-          label="Fornavn"
+        <BaseInput v-model="formData.firstName" type="text" label="Fornavn"
           :placeholder="v$.firstName.$error ? errorMessage : 'Fornavn'"
-          :class="v$.firstName.$error ? 'error-message' : ''"
-          class="input input--main"
-        />
+          :class="v$.firstName.$error ? 'error-message' : ''" class="input input--main" />
       </div>
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.lastName"
-          type="text"
-          label="Efternavn"
-          class="input input--main"
+        <BaseInput v-model="formData.lastName" type="text" label="Efternavn" class="input input--main"
           :placeholder="v$.firstName.$error ? errorMessage : 'Efternavn'"
-          :class="v$.lastName.$error ? 'error-message' : ''"
-        />
+          :class="v$.lastName.$error ? 'error-message' : ''" />
       </div>
     </div>
     <div class="input-wrapper input-wrapper--main">
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.zipCode"
-          type="number"
-          label="Postnummer"
-          placeholder="Postnummer"
-          class="input input--main"
-        />
+        <BaseInput v-model="formData.zipCode" type="number" label="Postnummer" placeholder="Postnummer"
+          class="input input--main" />
       </div>
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.email"
-          type="email"
-          label="Mail"
-          class="input input--main"
-          :placeholder="v$.firstName.$error ? errorMessage : 'Email'"
-          :class="v$.email.$error ? 'error-message' : ''"
-        />
+        <BaseInput v-model="formData.email" type="email" label="Mail" class="input input--main"
+          :placeholder="v$.firstName.$error ? errorMessage : 'Email'" :class="v$.email.$error ? 'error-message' : ''" />
       </div>
     </div>
     <div class="input-wrapper input-wrapper--main">
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.reEmail"
-          type="email"
-          label="Gentag email"
-          class="input input--main"
+        <BaseInput v-model="formData.reEmail" type="email" label="Gentag email" class="input input--main"
           :placeholder="v$.reEmail.$error ? errorMessage : 'Gentag email'"
-          :class="v$.email.$error ? 'error-message' : ''"
-        />
+          :class="v$.email.$error ? 'error-message' : ''" />
       </div>
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.phone"
-          type="number"
-          label="Mobilnummer"
-          placeholder="Mobilnummer"
-          class="input input--main"
-        />
+        <BaseInput v-model="formData.phone" type="number" label="Mobilnummer" placeholder="Mobilnummer"
+          class="input input--main" />
       </div>
     </div>
     <div class="input-wrapper input-wrapper--main">
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.adult"
-          type="text"
-          label="Antal voksne i husstanden"
-          placeholder="Antal i husstanden"
-          class="input input--main"
-        />
+        <BaseInput v-model="formData.adult" type="text" label="Antal voksne i husstanden"
+          placeholder="Antal i husstanden" class="input input--main" />
       </div>
       <div class="input-column input-column--main">
-        <BaseInput
-          v-model="formData.children"
-          type="text"
-          label="Antal børn i husstanden"
-          placeholder="Antal børn i husstanden"
-          class="input input--main"
-        />
+        <BaseInput v-model="formData.children" type="text" label="Antal børn i husstanden"
+          placeholder="Antal børn i husstanden" class="input input--main" />
       </div>
     </div>
 
     <div class="checkbox-wrapper">
       <input type="checkbox" id="agreeCheckbox" v-model="formData.agree" />
-      <label for="agreeCheckbox"
-        >Er du indforstået med, at I indsamler og behandler mine data i
+      <label for="agreeCheckbox">Er du indforstået med, at I indsamler og behandler mine data i
         overensstemmelse med jeres privatlivspolitik. Læs mere og vores cockie-
         og privatlivspolitik.
       </label>
     </div>
     <span class="error-span" v-if="!formData.agree && v$.agree.$dirty">
       <br />
-      Du skal acceptere betingelserne og vilkårene.</span
-    >
+      Du skal acceptere betingelserne og vilkårene.</span>
     <div class="button-container">
       <button type="submit" class="button button--main">Bliv medlem</button>
     </div>
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,46 +1,53 @@
-import '../firebase'
+import "../firebase";
 import {
-    getAuth,
-    onAuthStateChanged,
-    sendPasswordResetEmail,
-    signInWithEmailAndPassword,
-    signOut
-} from 'firebase/auth'
+	getAuth,
+	onAuthStateChanged,
+	sendPasswordResetEmail,
+	signInWithEmailAndPassword,
+	signOut,
+} from "firebase/auth";
 
-const auth = getAuth()
+const auth = getAuth();
 
 export const onSignOut = async () => {
-    try {
-        await signOut(auth)
-    } catch (e) {
-        console.log(e);
-    }
-}
+	try {
+		await signOut(auth);
+	} catch (e) {
+		console.log(e);
+	}
+};
 
 export const stateChange = (callback) => {
-    onAuthStateChanged(auth, callback)
-}
+	onAuthStateChanged(auth, callback);
+};
 
 export const signIn = async (email, password) => {
-    try {
-        const userData = await signInWithEmailAndPassword(auth, email, password)
-        console.log(userData.user);
+	try {
+		const userData = await signInWithEmailAndPassword(
+			auth,
+			email,
+			password
+		);
+		console.log(userData.user);
 
-        return {
-            uid: userData.user.uid
-        }
-    } catch (e) {
-        return console.log(e)
-    };
-}
+		return {
+			uid: userData.user.uid,
+		};
+	} catch (e) {
+		return console.log(e);
+	}
+};
 
 export const passwordReset = async (email) => {
-    sendPasswordResetEmail(auth, email)
-    .then(() => {
-      alert('password reset email sent')
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-}
+	sendPasswordResetEmail(auth, email)
+		.then(() => {
+			alert("password reset email sent");
+		})
+		.catch((error) => {
+			// sendPasswordResetEmail no longer sends error messages as per 15 sept 2023 (for safety, see link below)
+			// https://cloud.google.com/identity-platform/docs/admin/email-enumeration-protection
+			//   const errorCode = error.code;
+			//   const errorMessage = error.message;
+			alert("error sending password reset mail");
+		});
+};
