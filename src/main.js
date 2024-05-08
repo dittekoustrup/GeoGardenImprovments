ada47b4ab81d173234d42728fc2775206522f009
Annica <annif011098@gmail.com>
2024-05-08 16:07:06 +0200

    add scroll to top

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import router from "./router";
 
 const app = createApp(App);
 
+router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0);
+  next();
+});
+
 app.use(createPinia());
 app.use(router);
 
